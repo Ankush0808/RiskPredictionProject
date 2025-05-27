d9c8f104cf962ddc3d786b987823de871fadc91d
unknown <ankushtonde8@gmail.com>
2025-05-26 20:06:58 -0400

    covered secrets

--- Home.py
+++ Home.py
@@ -11,6 +11,8 @@ import os
 
 load_dotenv()  # Load environment variables from .env file
 
+import os
+
 aws_access_key = os.getenv("AWS_ACCESS_KEY_ID")
 aws_secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
 
